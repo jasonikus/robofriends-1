ad32dffd4bbd0d221524013da6e7dc4dd8181bc0
Jason George <jasonikus@gmail.com>
2020-03-20 18:22:35 -0500

    Updates

---
+++ precache-manifest.cd7f50824173c67963dc552c43391a7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a05ea2c394173ffd454d5e507ed327c",
+    "url": "/jasonikus/robofriends-1.git/index.html"
+  },
+  {
+    "revision": "b2a9767cb23eaf74c9c2",
+    "url": "/jasonikus/robofriends-1.git/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "03bfe4fafbd616a9872f",
+    "url": "/jasonikus/robofriends-1.git/static/css/main.dffa0968.chunk.css"
+  },
+  {
+    "revision": "b2a9767cb23eaf74c9c2",
+    "url": "/jasonikus/robofriends-1.git/static/js/2.5b396b40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jasonikus/robofriends-1.git/static/js/2.5b396b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03bfe4fafbd616a9872f",
+    "url": "/jasonikus/robofriends-1.git/static/js/main.9b2bfac0.chunk.js"
+  },
+  {
+    "revision": "43d6b800293830e4d2b2",
+    "url": "/jasonikus/robofriends-1.git/static/js/runtime-main.f5622e70.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/jasonikus/robofriends-1.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
